a05b7591278e9cb9cae6c6a6326e29c309be10e5
nagendraalavala <nagendra.alavala@gmail.com>
2020-07-07 11:29:56 -0500

    eureka

--- src/main/java/com/example/Users/MovieUsers/MovieUsersApplication.java
+++ src/main/java/com/example/Users/MovieUsers/MovieUsersApplication.java
@@ -2,6 +2,7 @@ package com.example.Users.MovieUsers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class MovieUsersApplication {
 
 	public static void main(String[] args) {
